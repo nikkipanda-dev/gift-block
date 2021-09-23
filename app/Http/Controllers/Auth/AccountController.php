411d79af4d2801534975349aad8c2aae744244e2
Nicole R <codebynikki@gmail.com>
2021-09-24 00:50:36 +0800

    Adds admin authentication

--- app/Http/Controllers/Auth/AccountController.php
+++ app/Http/Controllers/Auth/AccountController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
 
     public function store(Request $request)
     {
-
         $this->validate($request, [
             'first_name' => 'bail|required|string',
             'last_name' => 'bail|required|string',
@@ -29,6 +29,21 @@ class AccountController extends Controller
         $user->password = Hash::make($request->password);
 
         $user->save();
+
+        return response()->json($request->all());
+    }
+
+    public function authenticate(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'bail|required|email',
+            'pw' => 'bail|required',
+            'remember' => 'present',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->pw], $request->remember)) {
+            $request->session()->regenerate();
+        }
 
         return response()->json($request->all());
     }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,5 +1,54 @@
 @extends('layout.app')
 
 @section('content')
+<form action="{{ route('admin.auth') }}" method="post" id="admForm">
+    @csrf
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address:</label>
+      <input type="email" class="form-control" name="email" id="email" autocomplete="email">
+    </div>
+    <div class="mb-3">
+      <label for="pw" class="form-label">Password:</label>
+      <input type="password" class="form-control" name="pw" id="pw" autocomplete="new-password">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="remember">
+        <label class="form-check-label" for="remember">Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Log In</button>
+</form>
 
+<script>
+
+    window.addEventListener('load', admIdx);
+
+    function admIdx() {
+        const admForm = document.getElementById('admForm');
+
+        if (admForm) {
+            admForm.addEventListener('submit', admAuth);
+        }
+    }
+
+    function admAuth(e) {
+        e.preventDefault();
+
+        const admParam = new FormData();
+
+        admParam.append('email', this.email.value);
+        admParam.append('pw', this.pw.value);
+        admParam.append('remember', this.remember.checked);
+
+        axios.post(this.action, admParam)
+
+        .then (success => {
+            console.log(success);
+        })
+
+        .catch (error => {
+            console.log(error.response.data.errors);
+        });
+    }
+
+</script>
 @endsection
