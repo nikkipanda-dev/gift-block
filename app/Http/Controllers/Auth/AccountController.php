2b6b8f6a69ddaa15b6d38a991b91cacb5a33c542
Nicole R <codebynikki@gmail.com>
2021-09-23 23:52:55 +0800

    Creates new account

--- app/Http/Controllers/Auth/AccountController.php
+++ app/Http/Controllers/Auth/AccountController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
+
     public function store(Request $request)
     {
+
+        $this->validate($request, [
+            'first_name' => 'bail|required|string',
+            'last_name' => 'bail|required|string',
+            'email' => 'bail|required|email|unique:users,email',
+            'password' => 'bail|required|between:8,15|confirmed',
+            'password_confirmation' => 'required',
+        ]);
+
+        $user = new User();
+
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
         return response()->json($request->all());
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,19 @@
 @extends('layout.app')
 
 @section('content')
-<form>
+<form action="{{ route('register.store') }}" method="post" id="regForm">
     @csrf
     <div class="mb-3">
+        <label for="fname" class="form-label">First name:</label>
+        <input type="text" class="form-control" name="fname" id="fname" autocomplete="given-name">
+      </div>
+      <div class="mb-3">
+        <label for="lname" class="form-label">Last name:</label>
+        <input type="text" class="form-control" name="lname" id="lname" autocomplete="family-name">
+      </div>
+    <div class="mb-3">
       <label for="email" class="form-label">Email address:</label>
-      <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" autocomplete="email">
+      <input type="email" class="form-control" name="email" id="email" autocomplete="email">
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Password:</label>
@@ -17,4 +25,40 @@
       </div>
     <button type="submit" class="btn btn-primary">Register</button>
   </form>
+
+<script>
+    window.addEventListener('load', regLoad);
+
+    function regLoad() {
+        const regForm = document.getElementById('regForm');
+
+        if (regForm) {
+            regForm.addEventListener('submit', testfn)
+        }
+    }
+
+    function testfn(e) {
+        e.preventDefault();
+        console.log(this.fname.value);
+
+        const regParam = new FormData()
+
+        regParam.append('first_name', this.fname.value);
+        regParam.append('last_name', this.lname.value);
+        regParam.append('email', this.email.value);
+        regParam.append('password', this.password.value);
+        regParam.append('password_confirmation', this.password_confirmation.value);
+
+        axios.post(this.action, regParam)
+
+        .then (success => {
+            console.log(success)
+        })
+
+        .catch (error => {
+            console.log(error.response.data.errors)
+        });
+    }
+
+</script>
 @endsection
